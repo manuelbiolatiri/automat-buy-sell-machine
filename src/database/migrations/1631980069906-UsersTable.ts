aaeae16f53ff65321204fc7f57aee25ea8f70fc3
Emmanuel Biolatiri <manuellaide49@gmail.com>
2021-09-18 17:14:35 +0100

    migrations

---
+++ src/database/migrations/1631980069906-UsersTable.ts
@@ -0,0 +1,25 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UsersTable1631980069906 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "products" (
+                "id" bigint(20) NOT NULL AUTO_INCREMENT,
+                "amount_available" bigint(20) NOT NULL,
+                "cost" bigint(20) NOT NULL,
+                "product_name" varchar(255) NOT NULL,
+                "seller_id" bigint(20) NOT NULL,
+                "created_at" datetime DEFAULT CURRENT_TIMESTAMP,
+                "updated_at" datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+                PRIMARY KEY ("id"),
+                KEY "seller_id" ("seller_id"),
+                CONSTRAINT "product_table_3_ibfk_1" FOREIGN KEY ("seller_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+              )`
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "products"`);
+    }
+
+}
